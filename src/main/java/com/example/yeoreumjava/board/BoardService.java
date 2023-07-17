635f2602f1a2ba1e21e4a4596457f665ba8e04f1
klaus9267 <klaus9267@gmail.com>
2023-07-17 17:44:34 +0900

    controller, service test

---
+++ src/main/java/com/example/yeoreumjava/board/BoardService.java
@@ -0,0 +1,11 @@
+package com.example.yeoreumjava.board;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+    public String getAllBoards() {
+        return "hi";
+    }
+
+}
