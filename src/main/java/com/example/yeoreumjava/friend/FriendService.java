44069113e1a4cad353de01d348e0b2ca29c1a8ad
klaus9267 <klaus9267@gmail.com>
2023-09-03 04:28:31 +0900

    querydsl 설정

--- src/main/java/com/example/yeoreumjava/friend/FriendService.java
+++ src/main/java/com/example/yeoreumjava/friend/FriendService.java
@@ -1,6 +1,9 @@
 package com.example.yeoreumjava.friend;
 
+import com.example.yeoreumjava.friend.domain.Friend;
 import com.example.yeoreumjava.friend.repository.FriendRepository;
+import com.example.yeoreumjava.user.UserService;
+import com.example.yeoreumjava.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,4 +11,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class FriendService {
     private final FriendRepository friendRepository;
+    private final UserService userService;
+
+    public void applyFriend(Long senderId, Long receiverId) {
+        User sender = userService.findUserById(senderId);
+        User receiver = userService.findUserById(receiverId);
+
+        Friend friend = Friend.builder().sender(sender).receiver(receiver).build();
+        friendRepository.save(friend);
+    }
 }
