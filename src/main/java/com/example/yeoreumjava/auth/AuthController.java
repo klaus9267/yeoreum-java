f6647860d42b513df0fc259e21118134722c27d8
klaus9267 <klaus9267@gmail.com>
2023-10-11 22:22:29 +0900

    spring security 실습

--- src/main/java/com/example/yeoreumjava/auth/AuthController.java
+++ src/main/java/com/example/yeoreumjava/auth/AuthController.java
@@ -1,10 +1,29 @@
 package com.example.yeoreumjava.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+
+    @GetMapping("/test")
+    public ResponseEntity<String> greeting() {
+        return ResponseEntity.ok("token");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login() {
+        return ResponseEntity.ok("give token!!");
+    }
+    @PostMapping("/join")
+    public ResponseEntity<String> join() {
+        return ResponseEntity.ok("give token!!");
+    }
 }
---
+++ src/main/java/com/example/yeoreumjava/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.yeoreumjava.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf().disable()
+                           .cors()
+                           //                           .formLogin().disable()
+                           .and()
+                           .authorizeHttpRequests()
+                           .requestMatchers("/api/auth/login","/api/auth/join").permitAll()
+                           //                           .requestMatchers(HttpMethod.GET, "/api/auth").authenticated()
+
+                           .and()
+                           .sessionManagement()
+                           .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                           .and()
+                           .build();
+    }
+}
